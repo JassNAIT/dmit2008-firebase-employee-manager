7af9e94096f48fc2445f748a992ffc21cabf5326
Jaswinder Kaur <jkaur79@nait.ca>
2021-04-01 17:22:01 -0600

    firebase commit

---
+++ src/pages/404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+const PageNotFound = (props)=>{
+    return (
+        <header>
+        <h1>Page Not Found</h1>
+        <p>Add Button to return to Home Page</p>
+        </header>
+    );
+}
+
+export default PageNotFound;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react';   /*7.2K (gzipped: 2.9K)*/
+import styled from 'styled-components';
+
+import FormInput from './../components/forms/FormInput';
+import Button from './../components/buttons/Button';
+
+
+const LoginPageStyles = styled.header`
+max-width:380px;
+margin:6rem auto 0;
+h1{
+    font-size:2.25rem;
+}
+`
+const LoginPage = (props)=>{
+    return (
+        <LoginPageStyles>
+        <header><h1>Login Page</h1></header>
+        <FormInput label="Email" type="text"/>
+        <FormInput label="Password" type="password"/>
+        <Button label="Login" uiStyles="login"/>
+        </LoginPageStyles>
+    );
+}
+
+
+export default LoginPage;
